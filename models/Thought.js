1014c8d6c116198f08b3f29b79ca9df732268d72
Dyravuth Yorn <yorn.dyravuth@gmail.com>
2021-08-29 17:34:18 -0700

    scratch last commit, Thought.js was finished

---
+++ models/Thought.js
@@ -0,0 +1,50 @@
+const { Schema, model } = require("mongoose");
+
+const ThoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: "Please enter text!",
+    minLength: [1, "A minimum of 1 character required!"],
+    maxLength: [280, "Text cannot exceed 280 characters."],
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+    get: (now) => {
+      const month = now.getMonth();
+      const day = now.getDate();
+      const year = now.getFullYear();
+      let hour = now.getHours();
+      const minute = now.getMinutes();
+      let ampm;
+      if (hour > 12) {
+        ampm = "pm";
+        hour -= 12;
+      } else {
+        ampm = "am";
+      }
+
+      const timestamp = `${month}/${day}/${year} ${hour}:${minute}${ampm}`;
+
+      return timestamp;
+    },
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [
+    {
+      type: "ObjectId",
+      ref: "reactionSchema",
+    },
+  ],
+});
+
+ThoughtSchema.virtual("thoughtCount").get(function () {
+  return this.thoughts.length;
+});
+
+const Thought = model("Thought", ThoughtSchema);
+
+module.exports = Thought;
